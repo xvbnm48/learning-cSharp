ad1f1010f34cdd459f416338176dd4824fc298b1
fariz.wisnu <fariz.wisnu@bluebirdgroup.com>
2023-04-06 07:45:33 +0700

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using ConsoleApp1;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            // Sololearn.Belajar.Sololearn();
+            Sololearn.Belajar.SoloLearn3();
+            string idol = "Aruno nakanishi";
+            Console.WriteLine("hello " + idol);
+            // const int myNum = 15;
+            // // myNum = 20;
+            // Console.WriteLine(myNum);
+            int x = 5, y = 8, z = 10;
+            x += 3;
+            Console.WriteLine(x);
+            Console.WriteLine(x + y + z);
+                double num = 10.9;
+                int myInt = (int)num;
+                Console.WriteLine(myInt);
+                Console.WriteLine(Convert.ToString(x));
+            // Console.WriteLine("input your name oshi");
+            // string nameOshi = Console.ReadLine();
+            //
+            // Console.WriteLine("oshi lu " + nameOshi + " dan panjang nama oshi lu itu " + nameOshi.Length);
+            string firtsName = "fariz";
+            string lastName = "prananda";
+            string myName = $"my full name is: {firtsName} {lastName}";
+            Console.WriteLine(myName);
+
+            if (x <=  y)
+            {
+                Console.WriteLine("anjay");
+            }
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(myName);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Sololearn.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1 
+{
+    public class Sololearn
+    {
+        public class Belajar
+        {
+            public static void Sololearn()
+            {
+                int a = 8;
+                int b = ++a;
+                if (a > 5)
+                {
+                    b -= 3;
+                }
+                else
+                {
+                    b = 9;
+                }
+                Console.WriteLine(b);
+                // const double pi = 3.14;
+                // double radius = Double.Parse(Console.ReadLine());
+                // double area = pi * Math.Pow(radius, 2);
+                // Console.WriteLine(area);
+                // const double pi = 3.14;
+                // double radius;
+                // Console.WriteLine("masukkan jari-jari");
+                // radius = Convert.ToDouble(Console.ReadLine());
+                // double hasil = (int)(pi * 2 * radius);
+                // Console.WriteLine("luas lingkaran adalah: " + hasil);
+            }
+
+            public static void SoloLearn2()
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num == 1)
+                {
+                    Console.WriteLine("Easy");
+                } else if (num == 2)
+                {
+                    Console.WriteLine("Medium");
+                } else if (num == 3)
+                {
+                    Console.WriteLine("Hard");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option");
+                }
+            }
+            public static void SoloLearn3()
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine(i*80);
+                }
+            }
+        }
+    }
+    
+}
